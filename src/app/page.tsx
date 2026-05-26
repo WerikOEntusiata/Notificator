5d8f344fe1fe2f6ab3ebdec32d546d76d8028acd
[dyad] <git@dyad.sh>
2026-05-25 22:22:46 -0300

    [dyad] wrote 1 file(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import NotificationChat from '@/components/NotificationChat';
+import NotificationReceiver from '@/components/NotificationReceiver';
 import { MadeWithDyad } from '@/components/made-with-dyad';
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <NotificationChat />
+    <div className="h-screen flex flex-col">
+      <NotificationReceiver />
       <MadeWithDyad />
     </div>
   );
